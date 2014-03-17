a19f76a9da7c1d2f834b4ec7cb3f4da7894492df
Will Ashworth <pdxdevguy@gmail.com>
2014-03-17 11:58:13 -0700

    Initial commit
    - PHP code
    - README

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-tithumb-shortcode
+Timthumb Shortcode
 =================
 
-TimThumb shortcode for Wordpress
+Have you ever grown frustrated at the lack of easy, on-the-fly cropping and image manipulation options in Wordpress when authoring a post or a page? Wordpress' built-in tools are okay for simple pages, but when you need to get highly specific with pixels, crops, etc, Wordpress' tools just make things harder...rather than easier.
+
+This is a TimThumb shortcode for Wordpress. It's been tested and confirmed working up to Wordpress 3.6.1.
+
+All you need to do is copy the PHP code into your theme's `functions.php` file and you're good to go! Optionally, you may simply upload the PHP file via FTP or checkout this repo via Git and include the file into your `functions.php` file.
+
+I hope this has helped somebody out there and that it has been found useful!
+
+**Note:**
+
+*If any of you are able to test on higher versions and report back that it's working, that would be great. A split in priorities has had me preoccupied lately on other projects.*
---
+++ timthumb-shortcode.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * TimThumb Shortcode
+ */
+function timthumb_shortcode( $atts, $content = null ) {
+    extract( shortcode_atts( array(
+        'width' => '894',
+        'height' => '',
+        'crop' => '',
+        'filter' => '',
+        'sharpen' => '',
+        'zoom' => '',
+        'quality' => '90',
+        'align' => 'left',
+        'alt' => '',
+        'title' => '',
+        'caption' => '',
+        'link' => '',
+        'hspace' => '0',
+        'vspace' => '0',
+    ), $atts ) );
+
+    // Path to the timthumb.php directory
+    if($_SERVER['SERVER_PORT'] == '80'){
+        $protocol = 'http://';
+    }else{
+        $protocol = 'https://';
+    }
+    $path = $protocol . $_SERVER['HTTP_HOST'] . '/wp-content/themes/escalation';
+
+    $ext = strtolower(pathinfo(trim($content), PATHINFO_EXTENSION));
+
+    if (($ext == "jpg" || $ext == "jpeg" || $ext == "png" || $ext == "gif")) {
+        $myimage = trim($content);
+        if($width)   $myimage .= '&w='  . $width;
+        if($height)  $myimage .= '&h='  . $height;
+        if($crop)    $myimage .= '&a='  . $crop;
+        if($filter)  $myimage .= '&f='  . $filter;
+        if($sharpen) $myimage .= '&s='  . $sharpen;
+        if($zoom)    $myimage .= '&zc=' . $zoom;
+        if($quality) $myimage .= '&q='  . $quality;
+
+        if ($caption != "") {
+
+            if ($align == "center") $align = "wp-caption aligncenter";
+            if ($align == "left") $align = "wp-caption alignleft";
+            if ($align == "right") $align = "wp-caption alignright";
+
+            return '<div class="'.$align.'" style="width: '.$width.'px;"><img src="'.$path.'/timthumb.php?src='.$myimage.'" alt="'.$alt.'" title="'.$title.'" width="'.$width.'" /><p class="wp-caption-text">'.$caption.'</p></div>';
+
+        } else if ($caption == "") {
+
+            if ($align == "center") $align = "aligncenter";
+            if ($align == "left") $align = "alignleft";
+            if ($align == "right") $align = "alignright";
+            return '<img src="'.$path.'/timthumb.php?src='.$myimage.'" alt="'.$alt.'" title="'.$title.'" width="'.$width.'" hspace="'.$hspace.'" vspace="'.$vspace.'" class="'.$align.'" />';
+        }
+    }
+
+    else {
+        return '<img class="'.$align.'" src="'.$path.'/error.jpg" alt="Error Image" width="'.$width.'" />';
+    }
+}
+add_shortcode('image', 'timthumb_shortcode');
